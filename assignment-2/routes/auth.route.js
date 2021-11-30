132fb1b578cc368bf1dd504a66df66a7646bd6d1
harsh gandhi <harshgandhi043@gmail.com>
2021-12-01 00:24:25 +0530

    created signup api

---
+++ assignment-2/routes/auth.route.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+
+const { signup } = require("../controllers/auth.controller");
+const checkErrors = require("../middlewares/checkErrors");
+
+router.post(
+  "/signup",
+  [
+    check("username", "username is required")
+      .trim()
+      .escape()
+      .isLength({ min: 1 }),
+    check("password", "Password should be atleast 8 character")
+      .trim()
+      .isLength({
+        min: 8,
+      }),
+  ],
+  checkErrors,
+  signup
+);
+
+module.exports = router;
