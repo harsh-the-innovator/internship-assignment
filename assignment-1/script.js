0cb9189cb7ac8dc23619168fefb65bb67afa26a7
harsh gandhi <harshgandhi043@gmail.com>
2021-11-30 23:45:51 +0530

    made some changes in fitlering

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,6 +1,8 @@
 const selector = document.getElementById("date-filter");
 const bankDataContainer = document.getElementById("bank-data");
+const todaysDate = new Date();
 let bankData = [];
+
 async function fetchData() {
   let result = await fetch("https://www.gov.uk/bank-holidays.json");
   let data = await result.json();
@@ -19,27 +21,57 @@ async function fetchData() {
 
 document.onload = fetchData();
 
-function fiterByYesterday(eventsArray, yesterdayDate) {
+function fiterByYesterday(eventsArray) {
+  let yesterdayDate = new Date(todaysDate);
+  yesterdayDate.setDate(todaysDate.getDate() - 1);
   return eventsArray.filter((event) => {
     let eventDate = new Date(event.date);
     return eventDate === yesterdayDate;
   });
 }
 
-function filterByLastWeek(eventsArray) {}
+function filterByLastWeek(eventsArray) {
+  let dateWeekAgo = new Date(todaysDate);
+  dateWeekAgo.setDate(todaysDate.getDate() - 7);
+  return eventsArray.filter((event) => {
+    let eventDate = new Date(event.date);
+    if (
+      eventDate.getTime() >= dateWeekAgo.getTime() &&
+      eventDate.getTime() < todaysDate.getTime()
+    )
+      return true;
+    else return false;
+  });
+}
 
-function filterByLastMonth(eventsArray) {}
+function filterByLastMonth(eventsArray) {
+  let dateMonthAgo = new Date(todaysDate);
+  dateMonthAgo.setDate(todaysDate.getDate() - 30);
+  return eventsArray.filter((event) => {
+    let eventDate = new Date(event.date);
+    if (
+      eventDate.getTime() >= dateMonthAgo.getTime() &&
+      eventDate.getTime() < todaysDate.getTime()
+    )
+      return true;
+    else return false;
+  });
+}
 
 //add event listener for changing option
 selector.addEventListener("change", function (e) {
   const currentValue = e.target.value;
+  let filteredData = [];
   if (currentValue === "yesterday") {
-    let yesterdayDate = new Date(Date.now() - 864e5);
-    let filteredData = fiterByYesterday(bankData, yesterdayDate);
-    console.log(filteredData);
+    filteredData = fiterByYesterday(bankData);
   } else if (currentValue === "last-week") {
-    console.log("last week");
+    filteredData = filterByLastWeek(bankData);
   } else if (currentValue === "last-month") {
-    console.log("last month");
+    filteredData = filterByLastMonth(bankData);
   }
+  bankDataContainer.innerHTML = `<ul id="bank-data-list">
+  ${filteredData.map((item) => {
+    return `<li>${item.title} - ${item.date}</li>`;
+  })}
+</ul>`;
 });
